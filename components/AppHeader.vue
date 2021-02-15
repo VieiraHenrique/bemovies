ac1953629e3394843032cbfe03e01a20082e4334
Henrique Andrade Vieira <me.henriquevieira@gmail.com>
2021-02-15 12:13:20 +0100

    add responsive header

---
+++ components/AppHeader.vue
@@ -0,0 +1,33 @@
+<template>
+  <header class="header">
+    <div class="container">
+      <div class="header__logo">
+        <h1><i class="fas fa-film"></i> Be<span>Movies</span></h1>
+      </div>
+      <div class="header__menu">
+        <i @click="toggleMenu" class="fas fa-bars"></i>
+        <ul class="header__menu--main" :class="drawer ? 'open' : ''">
+          <li>Featured</li>
+          <li>Genres</li>
+          <li>Register</li>
+          <li>Login</li>
+        </ul>
+      </div>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      drawer: false,
+    }
+  },
+  methods: {
+    toggleMenu: function () {
+      this.drawer = !this.drawer
+    },
+  },
+}
+</script>
